ce0f3a2ee774b017cc8bcfc8b08177e887a5feb2
bob <robin.lilius@gmail.com>
2019-03-26 14:14:36 +0100

    Add: pushable boxes

---
+++ CIU235-Project/Assets/BoxPushedScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPushedScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    private BoxCollider bc;
+    //private GameObject character;
+    private bool moving;
+    private Vector3 direction;
+    private Vector3 next_pos;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        bc = GetComponent<BoxCollider>();
+        //character = GameObject.Find("Character");
+        direction = new Vector3();
+        next_pos = rb.position;
+        moving = false;
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    void FixedUpdate()
+    {
+        if (moving)
+        {
+            Vector3 cur_pos = rb.position;
+            rb.MovePosition(cur_pos + direction * speed * Time.deltaTime);
+            if ((direction.x > 0 && rb.position.x > next_pos.x) || (direction.x < 0 && rb.position.x < next_pos.x)
+                || (direction.z > 0 && rb.position.z > next_pos.z) || (direction.z < 0 && rb.position.z < next_pos.z))
+            {
+                //while(rb.position != next_pos)
+                //{
+                rb.MovePosition(next_pos);
+                //}
+                moving = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision? name: "+ other.gameObject.name);
+
+        if (!moving && other.gameObject.name == "Character")
+        {
+            GameObject c = other.gameObject;
+            CharacterControllerScript script = c.GetComponent<CharacterControllerScript>();
+            Vector3 cur_pos = rb.position;
+
+            direction = script.direction;
+            speed = script.speed;
+            //next_pos = new Vector3(cur_pos.x + script.grid_size * x, cur_pos.y + script.grid_size * y, cur_pos.z + script.grid_size * z);
+            next_pos = cur_pos + direction * script.grid_size;
+            moving = true;
+
+            //rb.MovePosition
+        }
+    }
+}
--- CIU235-Project/Assets/CharacterControllerScript.cs
+++ CIU235-Project/Assets/CharacterControllerScript.cs
@@ -11,7 +11,7 @@ public class CharacterControllerScript : MonoBehaviour
     private float move_delay;
 
     private bool moving;
-    private Vector3 direction;
+    public Vector3 direction;
     private Vector3 next_pos;
 
     public float speed;
@@ -24,6 +24,7 @@ public class CharacterControllerScript : MonoBehaviour
         move_delay = 0;
         moving = false;
         direction = new Vector3();
+        next_pos = rb.position;
     }
 
     // Update is called once per frame
@@ -32,42 +33,15 @@ public class CharacterControllerScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Debug.Log(Input.GetAxis("Vertical") + " - " + Input.GetAxis("Horizontal"));
-        //Vector3 pos = rb.position;
-        //pos.x += Input.GetAxis("Horizontal") * speed;
-        //pos.z += Input.GetAxis("Vertical") * speed;
-        //rb.MovePosition(pos);
-
-
-        //if (move_delay > 0) move_delay -= Time.deltaTime;
-        //if (move_delay <= 0)
-        //{
-        //    Vector3 grid_pos = getGridPos(pos.x, pos.y, pos.z);
-
-        //    Debug.Log("Grid pos 1: " + grid_pos +", axis horizontal: " + Input.GetAxis("Horizontal"));
-
-        //    if (Mathf.Abs(Input.GetAxis("Horizontal")) > EPSILON)
-        //    {
-        //        move_delay = DELAY_DEFAULT;
-        //        grid_pos.x += ((Input.GetAxis("Horizontal") > EPSILON) ? 1 : (Input.GetAxis("Horizontal") < EPSILON) ? -1 : 0) * grid_size;
-        //    }
-        //    if (Mathf.Abs(Input.GetAxis("Vertical")) > EPSILON)
-        //    {
-        //        move_delay = DELAY_DEFAULT;
-        //        grid_pos.z += ((Input.GetAxis("Vertical") > EPSILON) ? 1 : (Input.GetAxis("Vertical") < EPSILON) ? -1 : 0) * grid_size;
-        //    }
-
-        //    Debug.Log("Grid pos 2: " + grid_pos);
-
-        //    Vector3 dir = Vector3.Normalize(pos - grid_pos);
-
-        //    rb.MovePosition(pos + dir * speed);
-        //}
 
         Vector3 cur_pos = rb.position;
 
         if (!moving)
         {
+            //while (rb.position != next_pos)
+            //{
+            //    rb.MovePosition(next_pos);
+            //}
             if (Input.GetAxis("Horizontal") > 0) move(cur_pos, 1, 0, 0);
             if (Input.GetAxis("Horizontal") < 0) move(cur_pos, -1, 0, 0);
             if (Input.GetAxis("Vertical") > 0) move(cur_pos, 0, 0, 1);
@@ -76,12 +50,20 @@ public class CharacterControllerScript : MonoBehaviour
 
         if (moving)
         {
-            rb.MovePosition(cur_pos + direction * speed * Time.deltaTime);
-            if ((direction.x > 0 && rb.position.x > next_pos.x) || (direction.x < 0 && rb.position.x < next_pos.x)
-                || (direction.z > 0 && rb.position.z > next_pos.z) || (direction.z < 0 && rb.position.z < next_pos.z))
+            //rb.MovePosition(cur_pos + direction * speed * Time.deltaTime);
+            Vector3 new_pos = cur_pos + direction * speed * Time.deltaTime;
+            if ((direction.x > 0 && new_pos.x > next_pos.x) || (direction.x < 0 && new_pos.x < next_pos.x)
+                || (direction.z > 0 && new_pos.z > next_pos.z) || (direction.z < 0 && new_pos.z < next_pos.z))
             {
-                rb.MovePosition(next_pos);
+                //while(rb.position != next_pos)
+                //{
+                    rb.MovePosition(next_pos);
+                //}
                 moving = false;
+            }
+            else
+            {
+                rb.MovePosition(new_pos);
             }
         }
     }
