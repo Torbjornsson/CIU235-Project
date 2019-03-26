e5af0a55c38b6a62a29e47dc43c5c44eeb3fea3b
bob <robin.lilius@gmail.com>
2019-03-26 11:11:49 +0100

    Add: Simple character movement

---
+++ CIU235-Project/Assets/CharacterControllerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;
+    public float grid_size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxis("Vertical") + " - " + Input.GetAxis("Horizontal"));
+        Vector3 pos = rb.position;
+        pos.x += Input.GetAxis("Horizontal") * speed;
+        pos.z += Input.GetAxis("Vertical") * speed;
+        rb.MovePosition(pos);
+
+        //Debug.Log("");
+    }
+}
