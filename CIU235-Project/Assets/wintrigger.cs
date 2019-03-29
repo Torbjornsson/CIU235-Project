62d7a19121e935589d9206315589d7ac607023b2
Torbjornsson <david.torbjornsson@gmail.com>
2019-03-29 10:59:57 +0100

    Basic door

--- CIU235-Project/Assets/wintrigger.cs
+++ CIU235-Project/Assets/wintrigger.cs
@@ -6,16 +6,26 @@ public class wintrigger : MonoBehaviour
 {
     public bool activated;
     private GameMasterScript gameMasterScript;
+
+    private GameObject door;
     // Start is called before the first frame update
     void Start()
     {
         gameMasterScript = GameObject.Find("GameMaster").GetComponent<GameMasterScript>();
+        door = GameObject.Find("Door");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (activated)
+        {
+            door.SetActive(false);
+        }
+        else
+        {
+            door.SetActive(true);
+        }
     }
 
     void OnTriggerEnter(Collider other) {
