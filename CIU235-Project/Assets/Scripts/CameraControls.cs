cc18d36caaddbbfa684443a1ee0977460495cad6
Torbjornsson <david.torbjornsson@gmail.com>
2019-03-29 15:16:04 +0100

    TODO:fix rotation

--- CIU235-Project/Assets/Scripts/CameraControls.cs
+++ CIU235-Project/Assets/Scripts/CameraControls.cs
@@ -6,31 +6,59 @@ public class CameraControls : MonoBehaviour
 {
     public GameObject character;
 
-    private Transform t;
+    private Quaternion from, to;
 
     private Vector3 offset;
+
+    public float speed;
 
     private bool rotating;
     // Start is called before the first frame update
     void Start()
     {
+        
         character = GameObject.Find("Character");
 
         offset = transform.position - character.transform.position;
 
         rotating = false;
+
+        speed = 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (!rotating)
+        {
+            from = transform.rotation;
+            to = from;
+            if (Input.GetAxis("Rotate") > 0.5)
+            {
+                //Debug.Log(to.eulerAngles.y);
+                //transform.rotation = Quaternion.AngleAxis(90, Vector3.up);
+                //to.x = 0;
+                //to.z = 0;
+                //transform.RotateAround(character.transform.position, Vector3.up, 90.0f);
+                rotating = true;
+                
+            }
+        }
+        else
+        {
+            transform.rotation = Quaternion.RotateTowards(from, to, Time.deltaTime * speed);
+            if (transform.rotation == to)
+            {
+                rotating = false;
+            }
+        }
+
     }
 
     private void LateUpdate() 
     {
         transform.position = character.transform.position + offset;
-        
     }
 
     private void RotateC(Transform t1, Transform t2, float degrees, float rotatetime){
