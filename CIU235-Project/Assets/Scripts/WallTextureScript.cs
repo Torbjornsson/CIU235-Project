b6e33f6b53fc2c13b233b8b9256118918906f8b6
bob <robin.lilius@gmail.com>
2019-03-29 16:26:44 +0100

    Add: Temporary textures and colors for Floor, Boxes and Walls

---
+++ CIU235-Project/Assets/Scripts/WallTextureScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTextureScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Material m = gameObject.GetComponent<MeshRenderer>().material;
+        Transform t = gameObject.GetComponent<Transform>();
+        m.mainTextureScale = new Vector2(t.localScale.x, t.localScale.z);
+        Debug.Log("Scale: " + t.localScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
