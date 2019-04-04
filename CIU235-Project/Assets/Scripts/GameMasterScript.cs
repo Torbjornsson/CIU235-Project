da1865693de1de68245a29091752a5d033382ac4
Torbjornsson <david.torbjornsson@gmail.com>
2019-04-04 10:58:48 +0200

    reset level, start new and exit game

--- CIU235-Project/Assets/Scripts/GameMasterScript.cs
+++ CIU235-Project/Assets/Scripts/GameMasterScript.cs
@@ -5,22 +5,65 @@ using UnityEngine.SceneManagement;
 
 public class GameMasterScript : MonoBehaviour
 {
+    bool isAxisUsed;
     bool levelWin;
     // Start is called before the first frame update
     void Start()
     {
         levelWin = false;
+        isAxisUsed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxis("Reset") != 0){
+            if (!isAxisUsed){
+                isAxisUsed = true;
+                ResetLevel();
+            }
+        }
+        else if (Input.GetAxis("Reset") == 0)
+        {
+            isAxisUsed = false;
+        }
+        if (Input.GetAxis("Menu") != 0)
+        {
+            if (!isAxisUsed){
+                isAxisUsed = true;
+                Debug.Log("QUIT");
+                Quit();
+            }
+        }
+        else if(Input.GetAxis("Menu") == 0)
+        {
+            isAxisUsed = false;
+        }
     }
 
     public void LevelWin()
     {
         levelWin = true;
         Debug.Log("Start new level");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+        int bIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (bIndex >= SceneManager.sceneCountInBuildSettings){
+            Debug.Log("Congraturations U win tHe Games!?");
+        }
+        else
+        {
+            SceneManager.LoadScene(bIndex);
+        }
+        
+    }
+
+    public void ResetLevel()
+    {
+        Debug.Log("Reset level");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit(){
+        Debug.Log("Quit application");
+        Application.Quit();
     }
 }
