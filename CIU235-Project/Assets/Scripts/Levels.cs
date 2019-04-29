7e49fad646d0418602076af5bb798357ca99f19e
Torbjornsson <david.torbjornsson@gmail.com>
2019-04-29 16:25:15 +0200

    start with level select

---
+++ CIU235-Project/Assets/Scripts/Levels.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class Levels : MonoBehaviour
+{
+    public int nmr_scns;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nmr_scns = SceneManager.sceneCountInBuildSettings;
+
+        for (int i = 1; i < nmr_scns; i++){
+            GameObject btn = new GameObject();
+            btn.transform.SetParent(gameObject.transform);
+            btn.AddComponent<RectTransform>();
+            //btn.AddComponent<Button>();
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
