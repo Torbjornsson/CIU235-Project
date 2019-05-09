b3564a3008bc23f9a8045534a8d462e6b2690866
bob <robin.lilius@gmail.com>
2019-05-09 11:49:04 +0200

    Add: StatePackage

---
+++ CIU235-Project/Assets/Scripts/StatePackage.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class StatePackage
+{
+    private Hashtable objects = new Hashtable();
+
+    public StatePackage(GameObject obj)
+    {
+        AddObject(obj);
+    }
+    public StatePackage(GameObject obj, Vector3 pos)
+    {
+        AddObject(obj, pos);
+    }
+
+    public void AddObject(GameObject obj)
+    {
+        AddObject(obj, obj.GetComponent<Rigidbody>().position);
+    }
+    public void AddObject(GameObject obj, Vector3 pos)
+    {
+        objects.Add(obj, pos);
+    }
+
+    public void ResetState()
+    {
+        foreach (GameObject obj in objects.Keys)
+        {
+            obj.GetComponent<Rigidbody>().MovePosition((Vector3)objects[obj]);
+        }
+    }
+}
