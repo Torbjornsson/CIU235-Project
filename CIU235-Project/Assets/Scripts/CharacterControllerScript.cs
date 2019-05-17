12cb8f7e2c3acc8d15575e671fb80c005bd3eefe
bob <robin.lilius@gmail.com>
2019-05-17 14:55:45 +0200

    Put squeeze-mechanics in own method, to use in level transition

--- CIU235-Project/Assets/Scripts/CharacterControllerScript.cs
+++ CIU235-Project/Assets/Scripts/CharacterControllerScript.cs
@@ -165,11 +165,7 @@ public class CharacterControllerScript : Pusher
                 // For walking animation:
                 float distance = Vector3.Distance(rb.position, next_pos); // Should be between [0,1]
                 float height = (1 - SQUEEZE_SIZE) + Mathf.Abs(0.5f - distance) * SQUEEZE_SIZE * 2; // Only change constant, no terms here
-                Vector3 scale = new Vector3(1, height, 1); // Only height is affected
-                eye_trans.localScale = scale;
-                Vector3 eye_new_pos = eye_trans.position;
-                eye_new_pos.y = rb.position.y + height / 2.0f;
-                eye_trans.position = eye_new_pos;
+                Squeeze(height);
             }
         }
         if (Vector3.zero != direction)
@@ -191,6 +187,15 @@ public class CharacterControllerScript : Pusher
             intensity = 0;
             SetLight(intensity);
         }
+    }
+
+    public void Squeeze(float height)
+    {
+        Vector3 scale = new Vector3(1, height, 1); // Only height is affected
+        eye_trans.localScale = scale;
+        Vector3 eye_new_pos = eye_trans.position;
+        eye_new_pos.y = rb.position.y + height / 2.0f;
+        eye_trans.position = eye_new_pos;
     }
 
     // Updates facing of player
