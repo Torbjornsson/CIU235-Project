1f37e0a3439ed661dea5621e52cd44b01cf898ae
Torbjornsson <david.torbjornsson@gmail.com>
2019-05-23 15:05:09 +0200

    failstates for first lvl

---
+++ CIU235-Project/Assets/Scripts/FailTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailTrigger : MonoBehaviour
+{
+    public Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.tag == "Box")
+        {
+            Debug.Log("Reached failstate");
+            canvas.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.gameObject.tag == "Box")
+        {
+            Debug.Log("Undone failstate");
+            canvas.gameObject.SetActive(false);
+        }
+    }
+}
